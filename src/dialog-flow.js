cf0b5f918be1eefc260e0f2fd52853d116f298b4
Mark Partola <mark.partola@rentateam.ru>
2019-01-28 16:50:20 +0300

    Please enter the commit message for your changes. Lines starting

--- src/dialog-flow.js
+++ src/dialog-flow.js
@@ -4,6 +4,6 @@ module.exports = class {
   handle(request, response) {
     const agent = new WebhookClient({ request, response });
 
-    agent.add("Hello from node");
+    agent.add("Hello from node!");
   }
 };
