341eb5947f50ec0c48760bf397e0e50662fd0198
Mark Partola <mark_partola@epam.com>
2020-05-09 18:47:36 +0300

    Add docker-compose postgres config. Add app check connection

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Sequelize } from 'sequelize';
 
 @Module({
   imports: [],
@@ -8,3 +9,20 @@ import { AppService } from './app.service';
   providers: [AppService],
 })
 export class AppModule {}
+
+const sequelize = new Sequelize('hive_db', 'hive-db-user', 'hive-db-pass', {
+  dialect: 'postgres',
+  host: 'database',
+});
+
+// To check connection
+(async () => {
+  const err = await sequelize.authenticate().then(console.log);
+  if (err) {
+    return console.log(err);
+  }
+
+  const users = await sequelize.query('SELECT * FROM users');
+
+  console.log("Users: ", users);
+})();
